1fa6d98b8ce7580a568e31c54b945517be6431a6
uncleeesia <weiyang1112@hotmailcom>
2025-04-19 13:28:14 +0800

    change

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/api/hello")
-def hello():
+@app.route("/")
+def home():
     return jsonify(message="Hello from Flask!")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
