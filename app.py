1efa55d5856b9464fc467d861acbd389b850d004
uncleeesia <weiyang1112@hotmailcom>
2025-04-22 19:28:29 +0800

    add cors

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
 
-app = Flask(__name__)
+from flask import Flask 
+from flask_cors import CORS 
+  
+app = Flask(__name__) 
+CORS(app)
 
 @app.route("/")
+@cross_origin() # allow all domains to make requests
 def home():
     return "Hello from Flask!"
 
