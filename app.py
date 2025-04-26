80669d551382223938f62bf13d1b1487f3aeda11
uncleeesia <weiyang1112@hotmailcom>
2025-04-27 02:29:09 +0800

    drafting get preference api

--- app.py
+++ app.py
@@ -9,5 +9,16 @@ CORS(app)
 def home():
     return jsonify({"message": "Hello from Flask!"}),200
 
+@app.route("/api/getPreferences", methods=["GET"])
+def get_preferences():
+    # preferences = {
+    #     "theme": "dark",
+    #     "notifications": True,
+    #     "language": "en"
+    # }
+    
+    return jsonify({"data":{"preferences":{}}}), 200
+
 if __name__ == "__main__":
     app.run()
+    
