43e4449bc44bf4d8d189dfa49285b82c0d602631
uncleeesia <weiyang1112@hotmail.com>
2025-05-05 00:09:24 +0800

    formatted the file and change return message

--- app.py
+++ app.py
@@ -1,27 +1,19 @@
-import logging
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from flask_cors import CORS
 import psycopg2
 
 app = Flask(__name__)
 CORS(app)
 
-conn = psycopg2.connect("postgres://u8dd30h9brbpjq:pf3390a930040c9e19e9c5ce482dcfbf829ee969f2ed886456eef7dc3a9519e88@ce0lkuo944ch99.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d3tpurv791l296", sslmode='require')
+conn = psycopg2.connect(
+    "postgres://u8dd30h9brbpjq:pf3390a930040c9e19e9c5ce482dcfbf829ee969f2ed886456eef7dc3a9519e88@ce0lkuo944ch99.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d3tpurv791l296", sslmode='require')
 cursor = conn.cursor()
-logging.basicConfig(level=logging.INFO)
+
+
 @app.route("/", methods=["GET"])
 def home():
-    cursor.execute("SELECT 1")
-    myresult = cursor.fetchall()
-    app.logger.info(f"URL: {request.url}")
-    app.logger.info(f"Method: {request.method}")
-    app.logger.info(f"Headers: {request.headers}")
-    print(request.url)
-    print(request.method)
-    for x in myresult:
-        print(x)
-    
-    return jsonify({"message": "Hello from Flask!"}), 200
+    return jsonify({"message": "Backend Server Connected"}), 200
+
 
 @app.route("/api/routes", methods=["GET"])
 def list_routes():
@@ -34,6 +26,7 @@ def list_routes():
             'rule': str(rule)
         })
     return jsonify(output)
+
 
 @app.route("/api/getPreferences", methods=["GET"])
 def get_preferences():
@@ -50,13 +43,15 @@ def get_preferences():
     #     "reviews50": False,
     #     "reviews200": True
     # }
-    preferences={}
+    preferences = {}
     return jsonify({"preferences": preferences}), 200
+
 
 @app.route("/api/UpdatePreferences", methods=["POST"])
 def update_preferences(preferences):
     # logic to update
     return jsonify({"preferences": preferences}), 200
+
 
 @app.route("/api/getServices", methods=["GET"])
 def get_services():
@@ -125,12 +120,13 @@ def get_services():
             "image": "https://placehold.co/600x400",
         }
     ]
-    
+
     return jsonify({"services": services}), 200
+
 
 @app.route("/api/getReviews", methods=["GET"])
 def get_reviews():
-    #some logic to get reviews tagged to specific services
+    # some logic to get reviews tagged to specific services
     reviews = [
         {
             "id": 1,
@@ -139,28 +135,28 @@ def get_reviews():
             "rating": 5,
             "review": "Excellent service!",
             "date": "2023-10-01",
-        },{
+        }, {
             "id": 2,
             "serviceId": 1,
             "author": "alicia",
             "rating": 3,
             "review": "moderate service!",
             "date": "2023-10-01",
-        },{
+        }, {
             "id": 3,
             "serviceId": 1,
             "author": "coconut",
             "rating": 3,
             "review": "moderate service!",
             "date": "2023-10-01",
-        },{
+        }, {
             "id": 4,
             "serviceId": 1,
             "author": "apple",
             "rating": 3,
             "review": "moderate service!",
             "date": "2023-10-01",
-        },{
+        }, {
             "id": 5,
             "serviceId": 1,
             "author": "orange",
@@ -170,6 +166,6 @@ def get_reviews():
         }]
     return jsonify({"reviews": reviews}), 200
 
+
 if __name__ == "__main__":
     app.run()
-    
