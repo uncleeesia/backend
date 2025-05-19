9c78904818ec3745e05ffd22fbf54a03eda182ac
uncleeesia <weiyang1112@hotmail.com>
2025-05-19 17:30:12 +0800

    update

--- payment_controller.py
+++ payment_controller.py
@@ -15,54 +15,41 @@ class PaymentController():
         self.schema = str.strip(Schema_Name)
 
     def create_payment(self, payment_details: dict) -> Payment | Exception:
-        """"""
-
         result = None
 
         try:
+            if not payment_details:
+                raise Exception("Invalid or missing arguments.")
 
-            if payment_details:
-
-                pass
-
-            else:
-
-                raise Exception("Invalid or missing arguements.")
-
-            # Insert fake payment id to use model validate
+            # Validate input using PaymentCreate (no payment_id)
             pending_payment = Payment.model_validate(payment_details)
 
-            sql_command = sql.SQL("""INSERT INTO {}.payment (service_id, from_user_id, to_user_id, price, payment_timestamp, booking_timestamp) VALUES (%(service_id)s, %(from_user_id)s, %(to_user_id)s, %(price)s, %(payment_timestamp)s, %(booking_timestamp)s) RETURNING payment_id, service_id, from_user_id, to_user_id, price, payment_timestamp, booking_timestamp""").format(sql.Identifier(self.schema))
-            para = pending_payment.model_dump()
+            sql_command = sql.SQL("""
+                INSERT INTO {}.payment (service_id, from_user_id, to_user_id, price, payment_timestamp, booking_timestamp)
+                VALUES (%(service_id)s, %(from_user_id)s, %(to_user_id)s, %(price)s, %(payment_timestamp)s, %(booking_timestamp)s)
+                RETURNING payment_id, service_id, from_user_id, to_user_id, price, payment_timestamp, booking_timestamp
+            """).format(sql.Identifier(self.schema))
 
+            para = pending_payment.model_dump()
             callToDB_result = self.dbt.callToDB(sql_command, para)
 
-            if isinstance(callToDB_result, tuple):
-
-                pass
-
-            elif isinstance(callToDB_result, Exception):
-
+            if isinstance(callToDB_result, Exception):
                 raise callToDB_result
-
-            else:
-
+            elif not isinstance(callToDB_result, tuple):
                 raise Exception("Unable to create payment.")
 
             cols = ("payment_id", "service_id", "from_user_id", "to_user_id",
                     "price", "payment_timestamp", "booking_timestamp")
 
             data = dict(zip(cols, callToDB_result))
-
             result = Payment.model_validate(data)
 
         except Exception as e:
-
             result = e
 
         finally:
-
             return result
+
 
     def extract_payment_for_report(self, user_id: int | None = None) -> list[PaymentReport] | Exception:
         """"""
